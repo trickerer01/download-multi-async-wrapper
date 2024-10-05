354e31f6e91c5b8d220940359051cc481ad16767
trickerer01 <onlysuffering+1@gmail.com>
2024-10-05 21:09:26 +0700

    Simplify usage string

--- src/cmdargs.py
+++ src/cmdargs.py
@@ -88,16 +88,17 @@ def valid_categories_list(categories_str: str) -> list[str]:
 
 def parse_arglist(args: Sequence[str]) -> None:
     parser = ArgumentParser(add_help=False)
-    parser.add_argument('--help', action='help')
+    parser.usage = 'main.py -script PATH_TO_FILE [options...]'
+    parser.add_argument('--help', action='help', help='Print this message')
     parser.add_argument('--debug', action=ACTION_STORE_TRUE, help=HELP_DEBUG)
     parser.add_argument('--no-download', action=ACTION_STORE_TRUE, help=HELP_NO_DOWNLOAD)
     parser.add_argument('--no-update', action=ACTION_STORE_TRUE, help=HELP_NO_UPDATE)
     parser.add_argument('--install', action=ACTION_STORE_TRUE, help=HELP_INSTALL)
-    parser.add_argument('-ignore', metavar='#ARG,LEN', default=[], action=ACTION_APPEND, help=HELP_IGNORE_ARGUMENT, type=IgnoredArg)
-    parser.add_argument('-idlist', metavar='#CAT,DWN,IDS', default=[], action=ACTION_APPEND, help=HELP_IDLIST, type=CatDwnIds)
-    parser.add_argument('-categories', metavar='#L,I,S,T', default=[], help=HELP_CATEGORIES, type=valid_categories_list)
-    parser.add_argument('-downloaders', metavar='#L,I,S,T', default=DOWNLOADERS, help=HELP_DOWNLOADERS, type=valid_downloaders_list)
-    parser.add_argument('-script', metavar='#PATH_TO_FILE', required=True, help=HELP_SCRIPT_PATH, type=valid_file_path)
+    parser.add_argument('-ignore', metavar='ARG,LEN', default=[], action=ACTION_APPEND, help=HELP_IGNORE_ARGUMENT, type=IgnoredArg)
+    parser.add_argument('-idlist', metavar='CAT,DWN,IDS', default=[], action=ACTION_APPEND, help=HELP_IDLIST, type=CatDwnIds)
+    parser.add_argument('-categories', metavar='L,I,S,T', default=[], help=HELP_CATEGORIES, type=valid_categories_list)
+    parser.add_argument('-downloaders', metavar='L,I,S,T', default=DOWNLOADERS, help=HELP_DOWNLOADERS, type=valid_downloaders_list)
+    parser.add_argument('-script', metavar='PATH_TO_FILE', required=True, help=HELP_SCRIPT_PATH, type=valid_file_path)
 
     parsed = parser.parse_args(args)
     Config.debug = parsed.debug
